0aa47ee5c25733d5566ae5c2d9ab5c24f7e24c12
Aaron Kessler <kessler.aaron@gmail.com>
2019-03-05 12:03:05 -0500

    intial commit

---
+++ 01_load_data.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(readxl)
+
+#import ftm-supplied data for identified individuals and groups
+#as well as other individuals who are employed by those groups
+
+donors <- read_excel("processed_data/FTM-vaxcontributors_cleaned.xlsx")
+
+donors <- donors %>% 
+  clean_names() %>% 
+  remove_empty(c("rows", "cols"))
+
+names(donors)
+glimpse(donors)
+
+# summaries
+
+donors %>% 
+  group_by(contributor) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(desc(sumamount))
+
+donors %>% 
+  group_by(election_jurisdiction, candidate) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(desc(sumamount))
+
+donors %>% 
+  group_by(candidate, contributor) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(candidate, desc(sumamount))
+
+
+# summaries WITHOUT Empower Texans
+donors %>% 
+  filter(contributor != "EMPOWER TEXANS") %>% 
+  group_by(contributor) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(desc(sumamount))
+
+donors %>% 
+  filter(contributor != "EMPOWER TEXANS") %>% 
+  group_by(election_jurisdiction, candidate) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(desc(sumamount))
+
+donors %>% 
+  filter(contributor != "EMPOWER TEXANS") %>% 
+  group_by(election_jurisdiction, candidate, contributor) %>% 
+  summarise(num = n(), sumamount = sum(amount)) %>% 
+  arrange(candidate, desc(sumamount))
+
+
+
